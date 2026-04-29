88d91c7fabebec32ca9d0f61570d6d99ccb3b773
MuhmmedTamer <muhmmedelshnawy@gmail.com>
2026-04-29 23:50:51 +0300

    Test Changes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ function greet(name){
     return `Hello, ${name}!`
 }
 module.exports = greet;
-if(require.main === module){
-    console.log(greet("Muhmmed"));
-}
+// if(require.main === module){
+//     console.log(greet("Muhmmed"));
+// }
